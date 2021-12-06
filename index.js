28ac0b04aae7d55f5333d9946efd83dfce0f778a
adarsh-thedev <adarshpittu@gmail.com>
2021-12-06 10:41:35 +0530

    passport

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-const { Client, Intents } = require("discord.js");
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
-
-const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-});
-
-client.login(process.env.TOKEN);
-
-client.once("ready", () => {
-  console.log(`${client.user.username} has logged In`);
-});
-
-client.on("messageCreate", (message) => {
-  if (message.author.bot) return;
-
-  if (message.content === "hello") {
-    message.reply(`${message.author} Hello from the bot!`);
-  }
-});
+const cors = require("cors");
+const passport = require("passport");
+const DiscordStrategy = require("./discordStrategy");
 
 const app = express();
+
+app.use(cors());
+app.use(passport.initialize());
+
+app.get("/", (req, res) => {
+  return res.json("OK");
+});
+
+app.get("/auth", passport.authenticate("discord"));
+app.get(
+  "/auth/redirect",
+  passport.authenticate("discord", {
+    failureRedirect: "/",
+  }),
+  async (req, res) => {
+    return res.json();
+  }
+);
 
 app.listen(8000, () => console.info("App is listening on port 8000"));
