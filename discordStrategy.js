e826b4bafe487c51ac809a00a90162d4502de990
adarsh-thedev <adarshpittu@gmail.com>
2021-12-07 15:18:41 +0530

    modification

--- discordStrategy.js
+++ discordStrategy.js
@@ -1,7 +1,6 @@
 const DiscordStrategy = require("passport-discord").Strategy;
 const passport = require("passport");
 const scope = ["identify", "email", "guilds", "guilds.join"];
-const axios = require("axios");
 
 const DiscordOauth2 = require("discord-oauth2");
 const oauth = new DiscordOauth2({
@@ -17,24 +16,6 @@ passport.use(
       scope,
     },
     (a, r, p, d) => {
-      // console.log(a, r, d);
-      // axios(
-      //   `https://discord.com/api/guilds/914807760988811274/members/${p.id}`,
-      //   {
-      //     method: "put",
-      //     headers: {
-      //       Authorization:
-      //         "Bot OTE3MzYzMzM1NjM5NDc4Mjky.Ya3nLw.NTJqcsqlaPi_6gqcUmQo39Wu-2o",
-      //       "Content-Type": "application/json",
-      //     },
-      //     params: {
-      //       access_token: a,
-      //       roles: ["917384340445470750"],
-      //     },
-      //   }
-      // ).catch((err) => {
-      //   console.log("ERROR", err);
-      // });
       oauth
         .addMember({
           accessToken: a,
@@ -43,8 +24,14 @@ passport.use(
           userId: p.id,
           roles: ["917384340445470750"],
         })
-        .then(console.log);
-      return console.info(p);
+        .then((data) => {
+          console.log(data);
+          return data;
+        })
+        .catch((err) => {
+          console.log(err);
+          return err;
+        });
     }
   )
 );
